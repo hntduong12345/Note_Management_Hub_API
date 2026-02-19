993656e58cefbe20b187adb062a9e30f1edbde93
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-19 17:18:24 +0700

    feat: add attachment controller and service

--- src/main/java/com/notemanagement/NoteManagementHubAPI/services/interfaces/AttachmentService.java
+++ src/main/java/com/notemanagement/NoteManagementHubAPI/services/interfaces/AttachmentService.java
@@ -1,8 +1,13 @@
 package com.notemanagement.NoteManagementHubAPI.services.interfaces;
 
+import com.notemanagement.NoteManagementHubAPI.dtos.attachmentdtos.AttachmentResponse;
+import com.notemanagement.NoteManagementHubAPI.dtos.attachmentdtos.AttachmentUploadRequest;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
 public interface AttachmentService {
-     /*
-    public CompleteFuture<AttachmentDTO> uploadFile(UUID noteId, MultipartFile file);
-    public CompleteFuture deleteAttachment(UUID id);
-     */
+    public CompletableFuture<AttachmentResponse> linkAttachment(AttachmentUploadRequest request, UUID userId);
+    public CompletableFuture<Void> deleteAttachment(UUID id, UUID userId);
 }
